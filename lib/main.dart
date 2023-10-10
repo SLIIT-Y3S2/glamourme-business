45ecf9421a17acea321b4a79d093b68f61f18bd0
shavindaL <shavinda724@gmail.com>
2023-10-10 18:55:29 +0530

    Add authentication to business app

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:glamourmebusiness/constants.dart';
-import 'package:glamourmebusiness/screens/language_selection_screen.dart';
-import 'package:glamourmebusiness/screens/login_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:glamourmebusiness/firebase_options.dart';
+import 'package:glamourmebusiness/glamourme_business_app.dart';
+import 'package:logging/logging.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-int? initScreen;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences preferences = await SharedPreferences.getInstance();
-  initScreen = await preferences.getInt('initScreen');
-  await preferences.setInt('initScreen', 1); //if already shown -> 1 else 0
+
+  // Initialize logging configuration that logs all messages via print
+  Logger.root.level = Level.ALL; // defaults to Level.INFO
+  Logger.root.onRecord.listen((record) {
+    print('${record.loggerName} : ${record.level.name} ===> ${record.message}');
+  });
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GlammourMe Business',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: Color(kSeedColor),
-            primary: Color(
-              kPrimaryColor,
-            )),
-        useMaterial3: true,
-      ),
-      home: //const LanguageSelectionScreen());
-          initScreen == 0 || initScreen == null
-              ? const LanguageSelectionScreen()
-              : const LoginScreen(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const LoginScreen(),
-    );
+    return const GlamourMeBusinessApp();
   }
 }
