f48d44c0e04fa650e8188624cfd19f53718973e2
Sandaru <87793405+IT21001352@users.noreply.github.com>
2023-10-19 02:22:23 +0530

    removed business creation button from profile

--- lib/screens/profile_index_screen.dart
+++ lib/screens/profile_index_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:glamourmebusiness/globals.dart';
-import 'package:glamourmebusiness/screens/business_create_screen1.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:glamourmebusiness/blocs/authentication/authentication_bloc.dart';
 
@@ -21,18 +20,6 @@ class ProfileIndexScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('Profile Screen'),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to the new screen when the button is pressed.
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        const BusinessCreationBasicDetails(), // Use the SecondScreen widget from the imported file.
-                  ),
-                );
-              },
-              child: Text('Go to Second Screen'),
-            ),
             ElevatedButton(
               onPressed: () => _signOut(context),
               child: const Text('Sign out'),
